69a650cddcbbccad3f0e6a1f228b64bc5408277b
Sebastian Blum <sebast.blum@gmail.com>
2020-12-20 15:30:26 +0100

    runtime improvements and unit tests

--- ACTCV/actcv.py
+++ ACTCV/actcv.py
@@ -6,77 +6,110 @@ Created on Wed Aug 28 10:22:41 2019
 """
 import actr
 import pandas as pd
+import numpy as np
 from tqdm.auto import tqdm
+import time
+
+
 
 class ActCV():
 
-    def __init__(self, data, head, indexinput = 0):
+    def __init__(self, data, head, indexinput = 0, timebreak = 0.01):
         self.header =  []
-        self.statelist = []
-        self.state_start = []
-        self.time_offset = 0
+        self.starttime = 0
         self.header = head
         self.index = indexinput
+        self.timebreak = timebreak
+        self.statedict = {}
         try:
-	        self.data = data
+	        self.data = self.convert_data_frame(data)
         except:
             print(f"Not able to load {data}")
         actr.add_command("commit-to-visicon", self.commit_to_visicon, "load the current line in the visicon")
-       # actr.add_command("initialize-visicon", initialize_visicon, "run initialize the visicon")
 
-    def commit_to_visicon(self, indices, statelist):
-        state_current = statelist[indices]
+
+
+    def convert_data_frame(self, dataframe):
+        df = dataframe.replace(np.nan, 0, regex=True)
+        return df
+
+    def tuple_to_dict(self, namedtuple):
+        tupleasdict = namedtuple._asdict()
+        del tupleasdict["Index"]
+        return tupleasdict
+
+
+    def commit_to_visicon(self, index):
+        state = self.tuple_to_dict(self.statedict[index])
         actr.delete_all_visicon_features()        
-        for index, headname in enumerate(self.header):
-            value = state_current[index]
+        for head, value in state.items(): # enumerate durch tuple
+            value = np.float64(value)
             actr.add_visicon_features(['isa', 'visual-location', 
                                        'value', value,
-                                       'color', headname,
+                                       'color', head,
                                        'screen-x', 0, 
                                        'screen-y', 0, 
                                        'width', 50, 
                                        'height', 15])
 
-		
-    def set_states(timecolumnname):
-        # set start time
-	    time_start = self.data[timecolumnname][0]
+
+    def set_states(self, timecolumnname):
+        t0 = time.time()
+        # this needs to be dynamically adjustable later on
+        timecolumnname_index = 1
+        alarmnumbercolumn = 1
+        alarmactivecolumn = -1
+
+        # save parameters
+        offsetdict = {}
+        statedict = {}
+        tonelist = []
+        time_start = self.data[timecolumnname][0]
         time_last_current = time_start
 
         # set states
-        self.state_start = self.data.iloc[0] # the iloc seems critical
-        state_previous = self.state_start
-        self.statelist.append(state_previous.values.tolist())
+        index = 0 # using the index makes it slow
+        for startstate_tuple in self.data.iloc[0].to_frame().transpose().itertuples():
+            offsetdict[startstate_tuple.Index] = startstate_tuple.TIME
+            statedict[startstate_tuple.Index] = startstate_tuple
+            previousstate_tuple = startstate_tuple
 
-        for itertuple in data.itertuple():
-            #print(state_iter)
-            time_current = itertuple.timecolumnname # das geht wahrscheinlich nicht, weil es ein string ist
-            self.time_offset = time_current - starttime
-            self.time_offset = self.time_offset + starttime
+        for currentstate_tuple in self.data.itertuples():
+            time_current = currentstate_tuple[timecolumnname_index]
+            time_offset = ( time_current - time_start ) + self.starttime
             time_difference = time_current - time_last_current
 
-            if time_difference > timebreak:          
-                time_last_current = time_current
-                state_current = itertuple
-                previous_value = state_previous[alarmnumbercolumn]
-                current_value = state_current[alarmnumbercolumn]
-                alarm_active = state_current[alarmactivecolumn]
-                # use tuples and save time offset, index and self statelist. 
-        		# Then convert it to list and call the function for each entry
-                self.statelist.append(state_current.values.tolist()) # does it make sense to create a dictionary here?
-                self.index += 1
-                if previous_value != current_value and previous_value == 0.0 and alarm_active == 1.0:
-                    #inser into tone list
-            state_previous = state_current
+            if time_difference > self.timebreak:          
+                time_last_current = time_current                
+                current_alarmstate = currentstate_tuple.AUD_FwsNumber
+                previous_alarmstate = previousstate_tuple.AUD_FwsNumber
+
+                statedict[index] = currentstate_tuple
+                offsetdict[index] = time_offset
+                index += 1
+                if previous_alarmstate != current_alarmstate and previous_alarmstate == 0.0:
+                    tonelist.append(self.time_offset)
+                previous_alarmstate = current_alarmstate
+        print(f"set states took: { round(time.time()-t0, 5) } seconds")
+        return offsetdict, statedict, tonelist
 
 
-    def schedule_Visicon(self, alarmactivecolumn, alarmnumbercolumn, timecolumnname, freq, duration, starttime = 0, timebreak = 0.01):            
+    def schedule_Visicon(self, timecolumnname):
+        t0 = time.time()
+        offsetdict, statedict, _ = self.set_states(timecolumnname)
+        self.statedict = statedict
+        for key, value in statedict.items():    # kann ich schneller machen, da ich value nicht brauche      
+            timepoint = offsetdict.get(key)
+            actr.schedule_event(timepoint, "commit-to-visicon", params = [key], maintenance = True )
+        self.tuple_to_dict(self.statedict[0])
+        print(f"schedule visicon took: { round(time.time()-t0, 5) } seconds")
+
+
+
+    def schedule_Tone(self, alarmactivecolumn, alarmnumbercolumn, timecolumnname, freq, duration, starttime = 0, timebreak = 0.01):            
+        #offset_list, statelist = set_states()
         pass
-
-    def load_vision():
-        # loop through list
-        # actr.schedule_event(self.time_offset, "commit-to-visicon", params = [self.index, self.statelist], maintenance = True )     
         # loop through list
         # actr.new_tone_sound(freq, duration, self.time_offset) # input these variables
         # print(f" Tone scheduled at {self.time_offset}")     
-        pass
+
--- tests/test_actcv.py
+++ tests/test_actcv.py
@@ -5,21 +5,27 @@ import .actcv as actcv
 
 class TestActCV(object):
 
-  def test_(self):
-          expected_sales_data = pd.DataFrame(
-            {
-                "x": ["3", "4", "5", "6", "7", "8"],
-                "y": ["3", "4", "5", "6", "7", "8"],
-                "s": [-10, -2.4, 0, 3, 5.5, None]
-            
-            }
+  def test_tuple_to_dict(self):
+        input_tuple = Pandas(Index=0, Sebastian=1, Tim=2, Alina=3, Max=4, Peter=5)
+
+        expected_output = OrderedDict([('Sebastian', 1), ('Tim', 2), ('Alina', 3), ('Max', 4), ('Peter', 5)])
+        
+        observed_output = actcv.tuple_to_dict(input_tuple)
+
+        pd.testing.assert_frame_equal(
+            observed_sales_data.reset_index(drop=True),
+            expected_sales_data.reset_index(drop=True),
+            check_dtype=False,
         )
-        (
-            observed_data,
-            observed_data_grouped,
-        ) = actcv.function(
-            x, y, s
-        )
+
+
+  def test_set_states(self):
+        input_tuple = Pandas(Index=0, Sebastian=1, Tim=2, Alina=3, Max=4, Peter=5)
+
+        expected_output = OrderedDict([('Sebastian', 1), ('Tim', 2), ('Alina', 3), ('Max', 4), ('Peter', 5)])
+        
+        observed_offsetdict, observed_statedict = actcv.set_states(data)
+
         pd.testing.assert_frame_equal(
             observed_sales_data.reset_index(drop=True),
             expected_sales_data.reset_index(drop=True),
