fc55cf6295044f1e7a398d99bda28fddca950c50
Sebastian Blum <sebast.blum@gmail.com>
2020-12-25 16:25:15 +0100

    added todos

--- pyactcv/actcv.py
+++ pyactcv/actcv.py
@@ -1,33 +1,45 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 28 10:22:41 2019
+2020-12-23
 
-@author: Sebastian Blum
+@author: seblum
 """
-from . import actr
+
+import actr
+import pandas as pd
 import numpy as np
-#from tqdm.auto import tqdm
 import time
 
+'''ToDo
+- instert variable column names
+- insert data load check
+- insert docstrings
+'''
 
-class ActCV:
+
+class ActCV():
 
     def __init__(self, data, timecolumnname, indexinput = 0, timebreak = 0.01):
-        self.starttime = 0
         self.timecolumnname = timecolumnname
+        self.starttime = 0
         self.index = indexinput
         self.timebreak = timebreak
-        # dataframe.columns.get_loc("<col_name>")
         self.offsetdict, self.statedict, self.tonelist = {}, {}, []
         try:
 	        self.data = self.convert_data_frame(data)
         except:
             print(f"Not able to load {data}")
-        actr.add_command("commit-to-visicon", self.commit_to_visicon, "load the current line in the visicon")
+        actr.add_command("commit-to-visicon", self.commit_to_visicon, "load one row in the visicon")
 
 
 
     def convert_data_frame(self, dataframe):
+        # check thath self.timecolumnname is in header
+        #    tmp = tmp.where((pd.notnull(tmp)), None)
+
+        # Column names are not allowed to have brackets as using itertuples
+        # data.columns = data.columns.str.replace('[', '').str.replace(']', '')
+        # try - catch
         df = dataframe.replace(np.nan, 0, regex=True)
         return df
 
@@ -43,8 +55,9 @@ class ActCV:
     def commit_to_visicon(self, index):
         state = self.tuple_to_dict(self.statedict[index])
         actr.delete_all_visicon_features()        
-        for head, value in state.items(): # enumerate durch tuple
-            value = np.float64(value)
+        for head, value in state.items(): 
+            if isinstance(value, (int, float)):
+                value = np.float64(value)
             actr.add_visicon_features(['isa', 'visual-location', 
                                        'value', value,
                                        'color', head,
@@ -69,10 +82,10 @@ class ActCV:
         time_last_current = time_start
 
         # set states
-        index = 0 # using the index makes it slow
+        index = 0 # using the index makes it slow(er)
         for startstate_tuple in self.data.iloc[0].to_frame().transpose().itertuples():
-            offsetdict[startstate_tuple.Index] = startstate_tuple.TIME
-            statedict[startstate_tuple.Index] = startstate_tuple
+            offsetdict[index] = startstate_tuple.TIME
+            statedict[index] = startstate_tuple
             previousstate_tuple = startstate_tuple
 
         for currentstate_tuple in self.data.itertuples():
@@ -82,15 +95,16 @@ class ActCV:
 
             if time_difference > self.timebreak:          
                 time_last_current = time_current                
-                current_alarmstate = currentstate_tuple.AUD_FwsNumber
+                current_alarmstate = currentstate_tuple.AUD_FwsNumber # set dynamical to number
                 previous_alarmstate = previousstate_tuple.AUD_FwsNumber
 
                 statedict[index] = currentstate_tuple
                 offsetdict[index] = time_offset
                 index += 1
-                if previous_alarmstate != current_alarmstate and previous_alarmstate == 0.0:
+                if previous_alarmstate != current_alarmstate and previous_alarmstate == 0.0 and currentstate_tuple.AlarmActive == 1.0:
                     tonelist.append(time_offset)
-                previous_alarmstate = current_alarmstate
+
+                previousstate_tuple = currentstate_tuple
         return offsetdict, statedict, tonelist
 
 
@@ -98,14 +112,14 @@ class ActCV:
     def load_States(self):
         t0 = time.time()
         self.offsetdict, self.statedict, self.tonelist = self.set_states()
-        print(f"schedule states  took: { round(time.time()-t0, 5) } seconds")
+        print(f"load states took: { round(time.time()-t0, 5) } seconds")
 
 
 
     def schedule_Visicon(self):
         t0 = time.time()
-        for key in self.offsetdict:  
-            timepoint = key
+        for key, value in self.offsetdict.items():  
+            timepoint = value
             actr.schedule_event(timepoint, "commit-to-visicon", params = [key], maintenance = True ) # this impedes runtime
         print(f"schedule visicon took: { round(time.time()-t0, 5) } seconds")
 
@@ -115,6 +129,5 @@ class ActCV:
         t0 = time.time()
         for key in self.tonelist:
             actr.new_tone_sound(freq, duration, key) # this impedes runtime
-            #print(key)
-        print(f"schedule tone    took: { round(time.time()-t0, 5) } seconds")
+        print(f"schedule tone took: { round(time.time()-t0, 5) } seconds")
 
